9b92b75a2a467ccdd4d6afefc928d83aeb6e6f2e
danfirsten <dan.first.4444@gmail.com>
2024-07-21 11:15:02 -0700

    Defining The Road

--- selfdriving_car/main.js
+++ selfdriving_car/main.js
@@ -3,16 +3,24 @@ canvas.width=200;
 
 // ctx means context
 const ctx=canvas.getContext('2d');
-const car=new Car(100,100,30,50);
+const road=new Road(canvas.width/2, canvas.width*0.9);
+const car=new Car(road.getLanceCenter(1),100,30,50);
 car.draw(ctx);
 
 animate();
 
 function animate(){
     car.update();
-    
+
     canvas.height=window.innerHeight;
+
+    ctx.save();
+    ctx.translate(0,-car.y+canvas.height*0.7);
+
+    road.draw(ctx);
     car.draw(ctx);
+
+    ctx.restore();
     // calls animate function again and again
     requestAnimationFrame(animate);
 }
