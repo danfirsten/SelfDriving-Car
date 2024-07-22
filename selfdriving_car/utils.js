961fe808d24e9071e50deac34d126ba6dcf1baf7
danfirsten <dan.first.4444@gmail.com>
2024-07-21 20:51:27 -0700

    Collision Detection

--- selfdriving_car/utils.js
+++ selfdriving_car/utils.js
@@ -20,3 +20,22 @@ function getIntersection(A,B,C,D) {
         }
     }
 }
+
+function polysIntersect(poly1, poly2) {
+    for(let i=0;i<poly1.length;i++){
+        for(let j=0;j<poly2.length;j++){
+            // taking all the segments that make the first polygon and
+            // comparing them against every segment of the other polygon
+            const touch=getIntersection(
+                poly1[i],
+                poly1[(i+1)%poly1.length],
+                poly2[j],
+                poly2[(j+1)%poly2.length]
+            );
+            if(touch) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
